7d1e11512283f69b3f276e709adcbb2939a9087f
Jay Macdonald <jay.macdonald@uleth.ca>
2014-09-23 10:42:55 -0600

    Adding YOURLS plugin header

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name:	CAS Authentication
+Plugin URI:		https://github.com/jamac/yourls-cas-auth
+Description:	This plugin enables use of CAS for authentication
+Version:			0.1
+Author:				jamac
+Author URI:		https://github.com/jamac/
+*/
